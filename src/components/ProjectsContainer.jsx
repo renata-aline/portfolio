bad7c950408bd8f53a999cabfa8e5b85bb09d3cf
renata-aline <renatalinec@gmail.com>
2024-01-18 12:23:16 -0300

    codigo comentado

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,20 +1,22 @@
 import "../styles/components/projectscontainer.sass";
 
-const projects = [
-  {
-    id: "1",
-    titulo: "PL Contabilidade",
-    descricao: "Tecnologias utilizadas no projeto. HTML5, CSS3, JAVASCRIPT, REACTJS,",
-    capa: "/portifolio/public/assets/pl.png",
-    link: "https://renata-aline.github.io/pl-contabilidade/",
-  },
-  {
-    id: "2",
-    titulo: "Mais Bela",
-    descricao: "Tecnologias utilizadas no projeto. HTML5, CSS3, JAVASCRIPT, REACTJS, SASS",
-    capa: "/portifolio/public/assets/mais-bela.png",
-    link: "https://renata-aline.github.io/MaisBela/",
-  },
+// const projects = [
+//   {
+//     id: "1",
+//     titulo: "PL Contabilidade",
+//     descricao:
+//       "Tecnologias utilizadas no projeto. HTML5, CSS3, JAVASCRIPT, REACTJS,",
+//     capa: "portifolio/assets/pl.png",
+//     link: "https://renata-aline.github.io/pl-contabilidade/",
+//   },
+//   {
+//     id: "2",
+//     titulo: "Mais Bela",
+//     descricao:
+//       "Tecnologias utilizadas no projeto. HTML5, CSS3, JAVASCRIPT, REACTJS, SASS",
+//     capa: "portifolio/assets/mais-bela.png",
+//     link: "https://renata-aline.github.io/MaisBela/",
+//   },
   // {
   //   id: "3",
   //   titulo: "teste",
@@ -22,28 +24,24 @@ const projects = [
   //   capa: "portifolio/public/assets/mais-bela1.png",
   //   link: "https://renata-aline.github.io/pl-contabilidade/",
   // },
-
-
-];
+// ];
 
 export const ProjectsContainer = () => {
   return (
     <section className="projects-container">
       <h2>projetos</h2>
-      <div className="projects-grid">
+      {/* <div className="projects-grid">
         {projects.map((project) => (
-          <div className="projects-card">
-            {/* id={project.id} key={project.id}  */}
+          <div className="projects-card" id={project.id} key={project.id}>
             <h3>{project.titulo}</h3>
             <p>{project.descricao}</p>
-
             <img src={project.capa} alt="foto" />
             <a target="_blank" href={project.link} className="button">
               ver projeto
             </a>
           </div>
         ))}
-      </div>
+      </div> */}
 
       <a
         target="_blank"

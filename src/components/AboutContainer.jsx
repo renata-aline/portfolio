701b1fccee3336fdc0c152359edd957312e64074
renata <renatalinec@gmail.com>
2025-02-06 15:48:26 -0300

    add new feature

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,17 +3,21 @@ const AboutContainer = () => {
     <section className="about-container">
       <h2>Sobre Mim</h2>
       <p>
-        Bem vindo ao meu portifólio !
-        Sou Desenvolvedora e estou em constante aprendizado com objetivo de 
-        aperfeiçoar meu conhecimento e me torna especialista em  desenvolvimento web.
-         Estou disposta a ensinar e aprender com outros
-        profissionais. Nesse momento estou buscando oportunidade como
-        desenvolvedora front - end. Tenho conhecimento em HTML, CSS, SASS, JavaScript,
-        Reactjs e utilizo o Git para versionamento de código dos meus 
-        projetos de estudo e pessoal.
+        Bem-vindo ao meu portfólio! Sou desenvolvedora front-end e estou sempre
+        em busca de aprimoramento contínuo para me tornar especialista em
+        desenvolvimento web. Tenho paixão por tecnologia e estou aberta a
+        compartilhar conhecimentos, colaborar e aprender com outros
+        profissionais da área.
       </p>
       <p>
-        No meu tempo livre gosto de viajar,ler e praticar esporte ao ar livre.
+        Atualmente, estou em busca de uma oportunidade como desenvolvedora
+        front-end. Possuo experiência com HTML, CSS, SASS, JavaScript e
+        React.js, além de utilizar Git para versionamento de código em meus
+        projetos pessoais e acadêmicos.
+      </p>
+      <p>
+        No meu tempo livre, gosto de viajar, ler e praticar esportes ao ar
+        livre.
       </p>
     </section>
   );
